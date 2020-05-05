bc45bc3f26c7dd2fc7601f31ebeb7fc56920abd5
Thomas Lupin <thomas.lupin@epita.fr>
2020-05-05 15:16:40 +0200

    root_conf: fix links

---
+++ root_conf/copy.sh
@@ -0,0 +1,14 @@
+#/bin/sh
+
+files="/etc/NetworkManager/dispatcher.d/98-thetoto.sh
+       /etc/pam.d/fprint-auth
+       /etc/pulse/client.conf
+       /etc/udev/rules.d/98-thetoto.rules
+       /etc/mpd.conf"
+
+SCRIPT_PATH=$(realpath "$0")
+DIR_PATH=$(dirname "$SCRIPT_PATH")
+
+for file in $files; do
+    mkdir -p $(dirname "$DIR_PATH$file") && cp "$file" "$DIR_PATH$file"
+done
--- root_conf/etc/NetworkManager/dispatcher.d/98-thetoto.sh
+++ root_conf/etc/NetworkManager/dispatcher.d/98-thetoto.sh
@@ -1,1 +1,17 @@
-/etc/NetworkManager/dispatcher.d/98-thetoto.sh
+#!/bin/sh
+export $(xpub)
+
+interface=$1
+state=$2
+
+function notif() {
+    su ${XUSER} -c "notify-send $2 \"$1\""
+}
+
+
+
+if [ "$state" == "up" ] || [ "$state" == "down" ]; then
+    notif "$*"
+else
+    notif "$*"
+fi
