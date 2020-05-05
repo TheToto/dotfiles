d90a9b900b2318a09fa0580222332d40e324a9bd
Thomas Lupin <thomas.lupin@epita.fr>
2020-05-05 15:48:04 +0200

    root_conf: add environment vars

--- root_conf/copy.sh
+++ root_conf/copy.sh
@@ -4,7 +4,8 @@ files="/etc/NetworkManager/dispatcher.d/98-thetoto.sh
        /etc/pam.d/fprint-auth
        /etc/pulse/client.conf
        /etc/udev/rules.d/98-thetoto.rules
-       /etc/mpd.conf"
+       /etc/mpd.conf
+       /etc/environment"
 
 SCRIPT_PATH=$(realpath "$0")
 DIR_PATH=$(dirname "$SCRIPT_PATH")
