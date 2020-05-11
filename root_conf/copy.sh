0395df343e40b51c234c20874c0e7a8dba0cbfd9
Thomas Lupin <thomas.lupin@epita.fr>
2020-05-11 17:22:12 +0200

    mpd: move to user home

--- root_conf/copy.sh
+++ root_conf/copy.sh
@@ -2,9 +2,7 @@
 
 files="/etc/NetworkManager/dispatcher.d/98-thetoto.sh
        /etc/pam.d/fprint-auth
-       /etc/pulse/client.conf
        /etc/udev/rules.d/98-thetoto.rules
-       /etc/mpd.conf
        /etc/environment"
 
 SCRIPT_PATH=$(realpath "$0")
